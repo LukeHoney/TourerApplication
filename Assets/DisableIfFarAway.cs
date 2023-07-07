cfb81a55476d421a829edc3e7427044b8824b896
LukeHoney <2248102@swansea.ac.uk>
2023-07-07 15:02:21 +0100

    Add iteam Deactivation if far from player for icons

---
+++ Assets/DisableIfFarAway.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableIfFarAway : MonoBehaviour
+{
+    // Variables:
+
+    private GameObject itemActivatorObject;
+    private ItemActivator activationScript;
+
+    // --------------------------------------------------
+
+    void Start()
+    {
+        itemActivatorObject = GameObject.Find("ItemActivator");
+        activationScript = itemActivatorObject.GetComponent<ItemActivator>();
+
+        StartCoroutine("AddToList");
+    }
+
+    IEnumerator AddToList()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        activationScript.addList.Add(new ActivatorItem { item = this.gameObject });
+    }
+}
